833e5738a6ec07c14f9a861838787092a20fa5c6
Michael Tran <mrtran02@bu.edu>
2024-01-28 22:33:33 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -5,12 +5,18 @@ class X:
     def __repr__(self):
         return "X"
 
+    def evaluate(self, x):
+        return x
+    
 class Int:
     def __init__(self, i):
         self.i = i
     
     def __repr__(self):
         return str(self.i)
+    
+    def evaluate(self, x):
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
@@ -20,6 +26,9 @@ class Add:
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
     
+    def evaluate(self, x):
+        return self.p1.evaluate(x) + self.p2.evaluate(x)
+    
 class Sub:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -28,6 +37,9 @@ class Sub:
     def __repr__(self):
         return repr(self.p1) + " - " + repr(self.p2)
  
+    def evaluate(self, x):
+        return self.p1.evaluate(x) - self.p2.evaluate(x)
+    
 class Mul:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -42,6 +54,9 @@ class Mul:
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
     
+    def evaluate(self, x):
+        return self.p1.evaluate(x) * self.p2.evaluate(x)
+    
 class Div:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -55,6 +70,9 @@ class Div:
         if isinstance(self.p2, Add) or isinstance(self.p2, Sub):
             return repr(self.p1) + " / ( " + repr(self.p2) + " )"
         return repr(self.p1) + " / " + repr(self.p2)
+    
+    def evaluate(self, x):
+        return self.p1.evaluate(x) / self.p2.evaluate(x)
 
 
 
